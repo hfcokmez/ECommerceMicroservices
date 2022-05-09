cf887dfd13690550ca08c080f57d20b660c34a1d
hfcokmez <hfcokmez@hotmail.com>
2022-05-09 11:36:29 +0300

    update startup

--- Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
@@ -31,7 +31,6 @@ namespace FreeCourse.Services.Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //Dependency Injection Load Services
             services.LoadServices();
             services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
