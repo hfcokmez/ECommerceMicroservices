26869486fb80172da19940eb9dab0e5ea7999606
Faruk Çökmez <hfcokmez@gmail.com>
2022-05-09 15:46:59 +0300

    CoursesController implementation

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Services.Abstract;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    public class CoursesController : CustomBaseController
+    {
+        private readonly ICourseService _courseService;
+
+        public CoursesController(ICourseService courseService )
+        {
+            _courseService = courseService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _courseService.GetAllAsync();
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _courseService.GetByIdAsync(id);
+            return CreateActionResultInstance(response);
+        }
+
+        [Route("/api/[controller]/GetAllByUserId/{userId}")]
+        public async Task<IActionResult> GetAllByUserId(string userId)
+        {
+            var response = await _courseService.GetByIdAsync(userId);
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost ]
+        public async Task<IActionResult> Create(CourseCreateDto courseCreate)
+        {
+            var response = await _courseService.CreateAsync(courseCreate);
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(CourseUpdateDto courseUpdate)
+        {
+            var response = await _courseService.UpdateAsync(courseUpdate);
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpDelete ]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var response = await _courseService.DeleteAsync(id);
+            return CreateActionResultInstance(response);
+        }
+
+    }
+}
