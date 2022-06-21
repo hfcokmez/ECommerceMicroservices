2b9610896a4e17958b77722ef470df1c60c77520
Faruk Çökmez <hfcokmez@gmail.com>
2022-06-21 11:35:46 +0300

    Dtos and Object Mapping Configurations

---
+++ Services/Order/FreeCourse.Services.Order.Application/Dtos/OrderDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using FreeCourse.Services.Order.Domain.OrderAggregate;
+
+namespace FreeCourse.Services.Order.Application.Dtos
+{
+	public class OrderDto
+	{
+        public int Id { get; set; }
+        public DateTime CreatedTime { get; set; }
+		public AddressDto Address { get; set; }
+		public string BuyerId { get; set; }
+		public List<OrderItemDto> OrderItems { get; set; }
+	}
+}
+
