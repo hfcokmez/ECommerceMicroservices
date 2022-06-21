83da232e6a4e8e4c164132cb41ddf33378be85e1
Faruk Çökmez <hfcokmez@gmail.com>
2022-06-21 09:53:58 +0300

    Entities, ValueObjects and Aggregates are created. OrderDbContext migrations.

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FreeCourse.Services.Order.Domain.Core;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+	public class Order : Entity, IAggregateRoot
+	{
+        public DateTime CreatedTime { get; private set; }
+        public Address Address { get; private set; }
+        public string BuyerId { get; private set; }
+        private readonly List<OrderItem> _orderItems;
+
+        public Order(Address address, string buyerId)
+        {
+            Address = address;
+            BuyerId = buyerId;
+            CreatedTime = DateTime.Now;
+            _orderItems = new List<OrderItem>();
+        }
+
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+        public void AddOrderItem(string productId, string productName, decimal price, string pictureUrl)
+        {
+            var productExist = _orderItems.Any(x => x.ProductId == productId);
+            if (!productExist)
+            {
+                var orderItem = new OrderItem(productId, productName, pictureUrl, price);
+                _orderItems.Add(orderItem);
+            }
+        }
+
+        public decimal GetTotalPrice => _orderItems.Sum(x => x.Price);
+    }
+}
+
