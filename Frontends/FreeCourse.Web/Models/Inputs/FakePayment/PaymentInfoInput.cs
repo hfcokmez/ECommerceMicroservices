39fc945c3f2f49a06abf907ae44b629be98fbf82
Faruk Çökmez <hfcokmez@gmail.com>
2022-09-15 22:55:32 +0300

    Payment Objects are created

---
+++ Frontends/FreeCourse.Web/Models/Inputs/FakePayment/PaymentInfoInput.cs
@@ -0,0 +1,13 @@
+using System.Security.Cryptography;
+
+namespace FreeCourse.Web.Models.Inputs.FakePayment
+{
+    public class PaymentInfoInput
+    {
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
--- Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentsController.cs
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -1,3 +1,4 @@
+using FreeCourse.Services.FakePayment.Models;
 using FreeCourse.Shared.ControllerBases;
 using FreeCourse.Shared.Dtos;
 using Microsoft.AspNetCore.Mvc;
@@ -9,7 +10,7 @@ namespace FreeCourse.Services.FakePayment.Controllers
     public class FakePaymentsController : CustomBaseController
     {
         [HttpPost]
-        public IActionResult ReceivePayment()
+        public IActionResult ReceivePayment(PaymentDTO paymentDto)
         {
             return CreateActionResultInstance(Response<NoContent>.Success(200));
         }
